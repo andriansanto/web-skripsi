ebb95f6390636e3bba0fa5c100922b6b324fdf78
andriansanto <andriansanto.mb1@gmail.com>
2022-05-03 14:11:14 +0700

    add env

--- src/firebase.js
+++ src/firebase.js
@@ -12,14 +12,14 @@ import {
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCC8R2gju17eYDpkYsqs91zu3WtLX7KfL8",
-    authDomain: "skripsi-01-97646.firebaseapp.com",
-    databaseURL: "https://skripsi-01-97646-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "skripsi-01-97646",
-    storageBucket: "skripsi-01-97646.appspot.com",
-    messagingSenderId: "150037912656",
-    appId: "1:150037912656:web:f3b6277c5193cf7b6ec555",
-    measurementId: "G-YYWMGCHZHY"
+    apiKey: process.env.REACT_APP_FIREBASE_API,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_ST_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_M_ID
 };
 
 // initialize firebase
